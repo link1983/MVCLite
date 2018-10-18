88510f978153b154298cc1952a1c31e202864f22
link <link@office>
2018-10-18 18:12:45 +0800

    注册表单临时2

--- MVCLite/MVCLite/Controllers/UserController.cs
+++ MVCLite/MVCLite/Controllers/UserController.cs
@@ -8,6 +8,7 @@ namespace MVCLite.Controllers
 {
     public class UserController : BaseController
     {
+        private readonly DAL.User dal = new DAL.User();
         // GET: User
         public ActionResult Login()
         {
@@ -20,7 +21,30 @@ namespace MVCLite.Controllers
         [HttpPost]
         public ActionResult Register(string name,string password)
         {
-            return View();
+            if(string.IsNullOrEmpty(name)||name.Length>16||name.Length<3)
+            {
+                return Content("账号密码错误！");
+            }
+            if (string.IsNullOrEmpty(password) || password.Length > 16 || password.Length < 6)
+            {
+                return Content("账号密码错误！");
+            }
+
+            var model = new Models.User();
+            model.name = name;
+            model.password = password;
+            model.Modifytime = DateTime.Now;
+            model.AddTime = DateTime.Now;
+            model.status = 1;
+            var isSuccess = dal.Add(model);
+            if(isSuccess)
+            {
+                return Redirect("/");
+            }
+            else
+            {
+                return Content("注册失败");
+            }
         }
     }
 }
