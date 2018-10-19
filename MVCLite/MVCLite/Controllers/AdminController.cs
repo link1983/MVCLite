980f3f4c1c3ef293d4c6ca310f4930d2c9aeaafd
xasa <xasa@HURRICANE430S>
2018-10-20 01:13:53 +0800

    添加后台登录功能

---
+++ MVCLite/MVCLite/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLite.Controllers
+{
+    public class AdminController : BaseController
+    {
+        // GET: Admin
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string name,string password)
+        {
+            if((name??"")!="manage"||(password??"")!="manage")
+            {
+                return Content("账户或密码错误");
+            }
+            else
+            {
+                DBUtility.TicketTool.SetCookie(name, "manager");
+                return Redirect("/manage/home/index");
+            }
+        }
+    }
+}
