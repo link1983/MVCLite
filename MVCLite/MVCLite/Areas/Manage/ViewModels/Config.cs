2bc2bf0a6965bd652ba026a56d8b1ed3103797b9
xasa <xasa@HURRICANE430S>
2018-10-22 00:57:42 +0800

    添加后台设置功能-MVC注解验证1

---
+++ MVCLite/MVCLite/Areas/Manage/ViewModels/Config.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCLite.Areas.Manage.ViewModels
+{
+    public class Config
+    {
+        [Required]
+        public string stitle { set; get; }
+        public string name { set; get; }
+        public string surl { set; get; }
+
+        public string sentitle { set; get; }
+        public string skeywords { set; get; }
+
+        public string sdescription { set; get; }
+
+        public string s_qq { set; get; }
+
+        public string s_qqu { set; get; }
+        public string s_email { set; get; }
+        public string s_address { set; get; }
+        public string scopyright { set; get; }
+
+
+    }
+}
