a6126ecd4e562181d1e5042319e2da52afc5e8ee
link <link@office>
2018-10-23 17:21:20 +0800

    添加内容分类功能

---
+++ MVCLite/MVCLite/Areas/Manage/ViewModels/CatalogAdd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MVCLite.Areas.Manage.ViewModels
+{
+    public class CatalogAdd
+    {
+        [Required]
+        [StringLength(25, MinimumLength = 3, ErrorMessage = "长度在3到25之间")]
+        public string Name { set; get; }
+    }
+}
