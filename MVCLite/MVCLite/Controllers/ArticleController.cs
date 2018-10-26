2d2bab1e2df90c91841b72df138cf1707c2b49a1
link <link@office>
2018-10-26 15:21:30 +0800

    添加文章明细页

---
+++ MVCLite/MVCLite/Controllers/ArticleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVCLite.Controllers
+{
+    public class ArticleController:BaseController
+    {
+        private readonly DAL.Article dal = new DAL.Article();
+
+        public ActionResult Index(int ID)
+        {
+            var model = dal.GetModel(ID);
+            if(model==null)
+            {
+                return Content("文章不存在");
+            }
+            return View(model);
+        }
+    }
+}
