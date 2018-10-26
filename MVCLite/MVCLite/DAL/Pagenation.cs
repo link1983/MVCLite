6d2c297ed8fde640e73a5de793b5fb727b08c666
link <link@office>
2018-10-26 17:14:54 +0800

    添加分页功能1

---
+++ MVCLite/MVCLite/DAL/Pagenation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVCLite.Utility;
+
+namespace MVCLite.DAL
+{
+    public class Pagenation
+    {
+        public void ArticlePager()
+        {
+            int pageCount = (int) DbHelperSQLite.GetSingle("select count(*) from article");
+
+        }
+    }
+}
