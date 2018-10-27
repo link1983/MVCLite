e92fb6663d403221fe94e7346440e13a22b5f1a9
xasa <xasa@HURRICANE430S>
2018-10-27 11:35:38 +0800

    添加分页功能2

---
+++ MVCLite/MVCLite/BLL/ArticleBLL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace MVCLite.BLL
+{
+    public class ArticleBLL
+    {
+        private readonly DAL.Article dal = new DAL.Article();
+
+        public List<MVCLite.Models.Article> GetModelList(string strWhere)
+        {
+            DataSet ds = dal.GetList(strWhere);
+            return DataTableToList(ds.Tables[0]);
+        }
+        /// <summary>
+        /// 获得数据列表
+        /// </summary>
+        public List<MVCLite.Models.Article> DataTableToList(DataTable dt)
+        {
+            List<MVCLite.Models.Article> modelList = new List<MVCLite.Models.Article>();
+            int rowsCount = dt.Rows.Count;
+            if (rowsCount > 0)
+            {
+                MVCLite.Models.Article model;
+                for (int n = 0; n < rowsCount; n++)
+                {
+                    model = dal.DataRowToModel(dt.Rows[n]);
+                    if (model != null)
+                    {
+                        modelList.Add(model);
+                    }
+                }
+            }
+            return modelList;
+        }
+    }
+}
